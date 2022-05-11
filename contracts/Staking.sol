80cc82f51a28bd222e6c29fc0acd98cb9a5ddcc1
mbigant <mickael.bigant@protonmail.com>
2022-05-11 22:40:39 +0200

    doc : add comments

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -6,9 +6,13 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Staking is Ownable {
 
+    /// List of pool
     Pool[] public pools;
+
+    /// @dev Mapping of Users in a pool
     mapping(uint => mapping(address => User)) private usersInPool;
 
+    /// @dev User who stack
     struct User {
         uint balance;
         uint rewardBalance;
@@ -16,6 +20,7 @@ contract Staking is Ownable {
         uint rewardPerToken;
     }
 
+    /// @dev Pool details
     struct Pool {
         uint totalStaked;
         uint lastUpdateBlock;
@@ -34,10 +39,27 @@ contract Staking is Ownable {
 
     }
 
-    function createPool(address _tokenToStake, address _tokenReward, uint _rewardPerBlock, address _chainlinkDataFeedAddress) external onlyOwner {
+    /**
+     * @dev Create a new pool
+     *
+     * @param _tokenToStake address of the staked token
+     * @param _tokenReward address of the rewarded token
+     * @param _rewardPerBlock how much _tokenReward will be distributed per block
+     * @param _rewardTokenDataFeedAddress chainlink datafeed address to get the price of the _tokenReward
+     *
+     * Emit a PoolCreated event
+     *
+     * Requirements:
+     * - `msg.sender` must be the owner
+     * - `_tokenToStake` must be a valid address
+     * - `_tokenReward` must be a valid address
+     * - `_rewardTokenDataFeedAddress` must be a valid address
+     * - `_rewardPerBlock` must be greater than zero
+     */
+    function createPool(address _tokenToStake, address _tokenReward, uint _rewardPerBlock, address _rewardTokenDataFeedAddress) external onlyOwner {
         require(_tokenToStake != address(0), "Bad staking token address");
         require(_tokenReward != address(0), "Bad reward token address");
-        require(_chainlinkDataFeedAddress != address(0), "Bad oracle address");
+        require(_rewardTokenDataFeedAddress != address(0), "Bad oracle address");
         require(_rewardPerBlock > 0, "Reward rate must be positive");
 
         pools.push(
@@ -54,6 +76,20 @@ contract Staking is Ownable {
         emit PoolCreated(pools.length - 1);
     }
 
+    /**
+     * @dev Deposit some token in the pool _poolId and update the rewards calculation
+     * @notice Deposit some tokens in a pool
+     *
+     * @param _poolId index of the pool in the Pool[] array
+     * @param _amount Number of token user want to deposit in the pool
+     *
+     * Emit a Staked event
+     *
+     * Requirements:
+     * - `_poolId` must exists in the Pool[] array
+     * - `_amount` must be greater than zero
+     * - `msg.sender` must own enough token to deposit
+     */
     function deposit(uint _poolId, uint _amount) external {
         require(_amount > 0, "You must deposit tokens");
         require(_poolId < pools.length, "Pool not found");
@@ -70,8 +106,23 @@ contract Staking is Ownable {
         emit Staked(_poolId, msg.sender, _amount);
     }
 
+    /**
+     * @dev Withdraw some token from the pool _poolId, update the rewards calculation and claim reward if possible
+     * @notice Withdraw some tokens from the pool
+     *
+     * @param _poolId index of the pool in the Pool[] array
+     * @param _amount Number of token user want to withdraw from the pool
+     *
+     * Emit a Withdrawn event
+     *
+     * Requirements:
+     * - `_poolId` must exists in the Pool[] array
+     * - `_amount` must be greater than zero
+     * - `msg.sender` must have enough token in this pool
+     */
     function withdraw(uint _poolId, uint _amount) external {
         require(_poolId < pools.length, "Pool not found");
+        require(_amount > 0, "Invalid token amount");
         require(_amount <= usersInPool[_poolId][msg.sender].balance, "Not enough token to widthdraw");
 
         _updateRewards(_poolId);
@@ -86,17 +137,43 @@ contract Staking is Ownable {
         emit Withdrawn(_poolId, msg.sender, _amount);
     }
 
+    /**
+     * @dev Claim rewards from the pool _poolId
+     * @notice Claim rewards
+     *
+     * @param _poolId index of the pool in the Pool[] array
+     *
+     * Emit a Claimed event
+     *
+     * Requirements:
+     * - `_poolId` must exists in the Pool[] array
+     * - `msg.sender` must have rewards to claim for
+     */
     function claim(uint _poolId) external {
         require(_poolId < pools.length, "Pool not found");
         require(usersInPool[_poolId][msg.sender].rewardBalance > 0, "Nothing to claim");
         _claimReward(_poolId);
     }
 
+    /**
+     * @dev Return the number of claimable tokens from the pool _poolId
+     *
+     * @param _poolId index of the pool in the Pool[] array
+     *
+     * Requirements:
+     * - `_poolId` must exists in the Pool[] array
+     */
     function getRewardToClaim(uint _poolId) external view returns (uint) {
         require(_poolId < pools.length, "Pool not found");
         return usersInPool[_poolId][msg.sender].rewardBalance;
     }
 
+    /**
+     * @dev Update the rewardPerToken of the pool _poolId
+     *
+     * @param _poolId index of the pool in the Pool[] array
+     *
+     */
     function _updateRewards(uint _poolId) private {
         pools[_poolId].rewardPerToken = _getRewardPerToken(_poolId);
         pools[_poolId].lastUpdateBlock = block.number;
@@ -104,10 +181,19 @@ contract Staking is Ownable {
         usersInPool[_poolId][msg.sender].rewardPerToken = pools[_poolId].rewardPerToken;
     }
 
+    /**
+     * @dev Return the result of the earned reward of the user
+     *
+     * @param _poolId index of the pool in the Pool[] array
+     *
+     */
     function _getRewardsEarnedLastPeriod(uint _poolId) private view returns (uint) {
         return (usersInPool[_poolId][msg.sender].balance * (pools[_poolId].rewardPerToken - usersInPool[_poolId][msg.sender].rewardPerToken) / 1e18) + usersInPool[_poolId][msg.sender].rewardBalance;
     }
 
+    /**
+     * @dev Send the reward amount and reset the balance
+     */
     function _claimReward(uint _poolId) private {
         if (usersInPool[_poolId][msg.sender].rewardBalance > 0) {
             uint rewardAmount = usersInPool[_poolId][msg.sender].rewardBalance;
@@ -119,6 +205,9 @@ contract Staking is Ownable {
         }
     }
 
+    /**
+     * @dev Return the result of reward per token since the last update
+     */
     function _getRewardPerToken(uint _poolId) private view returns (uint) {
         if (pools[_poolId].totalStaked == 0) {
             return pools[_poolId].rewardPerToken;
@@ -128,20 +217,37 @@ contract Staking is Ownable {
         }
     }
 
-    function getRemainingRewards(uint _poolId) external view returns (uint) {
-        return pools[_poolId].rewardToken.balanceOf(address(this));
-    }
-
-    function getUpdatedRewardPerToken(uint _poolId) external returns (uint) {
-        _updateRewards(_poolId);
-        return pools[_poolId].rewardPerToken;
-    }
-
+    /**
+     * @dev Return the user balance of deposited token in the pool _poolId
+     *
+     * @param _poolId index of the pool in the Pool[] array
+     * @param _address user address
+     *
+     * Requirements:
+     * - `_poolId` must exists in the Pool[] array
+     */
     function getUserBalance(uint _poolId, address _address) external view returns (uint) {
+        require(_poolId < pools.length, "Pool not found");
         return usersInPool[_poolId][_address].balance;
     }
 
+    /**
+     * @dev Return the total balance of the pool _poolId
+     *
+     * @param _poolId index of the pool in the Pool[] array
+     *
+     * Requirements:
+     * - `_poolId` must exists in the Pool[] array
+     */
     function getPoolBalance(uint _poolId) external view returns (uint) {
+        require(_poolId < pools.length, "Pool not found");
         return pools[_poolId].totalStaked;
     }
+
+    // todo garder ? permettrait de savoir si la pool a assez de reward token
+    function getRemainingRewards(uint _poolId) external view onlyOwner returns (uint) {
+        require(_poolId < pools.length, "Pool not found");
+        return pools[_poolId].rewardToken.balanceOf(address(this));
+    }
+
 }
