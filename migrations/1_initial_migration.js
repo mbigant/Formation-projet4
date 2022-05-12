45aaea32a38964060b0d198464fa376907954b77
mbigant <mickael.bigant@protonmail.com>
2022-05-12 20:35:28 +0200

    fix : bug migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,7 +2,6 @@ var Staking = artifacts.require("./Staking.sol");
 var RewardToken = artifacts.require("./RewardToken.sol");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(RewardToken, 1000000).then( () => {
-    deployer.deploy(Staking);
-  });
+  deployer.deploy(RewardToken, 1000000);
+  deployer.deploy(Staking);
 };
