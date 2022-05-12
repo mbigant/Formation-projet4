22b2f762d4a95c473c6f1b6b5cddf65cd15e3599
mbigant <mickael.bigant@protonmail.com>
2022-05-12 15:52:58 +0200

    feat : add chainlink datafeed

--- contracts/PriceConsumerV3.sol
+++ contracts/PriceConsumerV3.sol
@@ -8,12 +8,16 @@ contract PriceConsumerV3 {
     AggregatorV3Interface internal immutable priceFeed;
 
     /**
-     * Network: Kovan
-     * Aggregator: ETH/USD
-     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     * https://docs.chain.link/docs/ethereum-addresses/
+     *
+     * Exemple :
+     * Kovan ETH/USD 0x9326BFA02ADD2366b30bacB125260Af641031331
+     * Kovan DAI/USD 0x777A68032a88E5A84678A77Af2CD65A7b3c0775a
+     * Kovan USDC/USD 0x9211c6b3BF41A10F78539810Cf5c64e1BB78Ec60
+     * Kovan XAU/USD 0xc8fb5684f2707C82f28595dEaC017Bfdf44EE9c5
      */
-    constructor() {
-        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+    constructor(address _datafeed) {
+        priceFeed = AggregatorV3Interface(_datafeed);
     }
 
     /**
